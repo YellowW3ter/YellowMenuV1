b9be7e1c3f71010b140b1d70c198857b9782446d
YellowWater <d85080941@gmail.com>
2025-04-02 21:17:06 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.Yellowwater.gorillatag.YellowWatertemplate";
+        public const string Name = "Yellow MenuV1";
+        public const string Description = "Made By @yelloww3ter on YT";
+        public const string Version = "1.0.0";
+    }
+}
